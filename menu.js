af51998c5845ee1e8c8d43d609cd9fc980b11ae6
ANDERSON REIS GONÇALVES <22282173@CFP127ALUNO.LOCAL>
2023-01-26 13:25:51 -0300

    Ajustes

--- menu.js
+++ menu.js
@@ -2,7 +2,12 @@ var menu = document.querySelector(".menuBar");
 var btn = document.querySelector(".btn-menu");
 var btnVoltar = document.querySelector(".btnVoltar");
 var paredeFechar = document.querySelector(".paredeFechar");
-var linkMenu = document.querySelector(".linkMenu");
+var linkMenu1 = document.querySelector(".linkMenu1");
+var linkMenu2 = document.querySelector(".linkMenu2");
+var linkMenu3 = document.querySelector(".linkMenu3");
+var linkMenu4 = document.querySelector(".linkMenu4");
+var linkMenu5 = document.querySelector(".linkMenu5");
+var linkMenu6 = document.querySelector(".linkMenu6");
 btn.addEventListener('click', () => {
     menu.classList.toggle('active');
     paredeFechar.classList.toggle('active');
@@ -21,9 +26,34 @@ paredeFechar.addEventListener('click', () => {
 
 
 })
-linkMenu.addEventListener('click', () => {
+linkMenu1.addEventListener('click', () => {
     menu.classList.toggle('active');
     paredeFechar.classList.toggle('active');
 
 
 })
+linkMenu2.addEventListener('click', () => {
+    menu.classList.toggle('active');
+    paredeFechar.classList.toggle('active');
+
+
+})
+linkMenu3.addEventListener('click', () => {
+    menu.classList.toggle('active');
+    paredeFechar.classList.toggle('active');
+
+
+})
+linkMenu4.addEventListener('click', () => {
+    menu.classList.toggle('active');
+    paredeFechar.classList.toggle('active');
+
+
+})
+linkMenu5.addEventListener('click', () => {
+    menu.classList.toggle('active');
+    paredeFechar.classList.toggle('active');
+
+
+})
+
